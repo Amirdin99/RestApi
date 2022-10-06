c3afd05bee20c4174f037a8372a46b2242887c5c
Amirdin99 <amirdinamirdin1999g@gmail.com>
2022-10-06 09:50:02 +0500

    project added to git

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class MainPage extends StatefulWidget {
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Api"),
+      ),
+
+      body: Container(
+
+      ),
+
+    );
+  }
+}
